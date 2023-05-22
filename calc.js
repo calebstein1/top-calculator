12baa7858697a26525e5c4058802475a5a9ecb8a
Caleb Stein <caleb@calebstein.net>
2023-05-22 16:10:26 -0700

    added arithmatic and history display

--- calc.js
+++ calc.js
@@ -1,25 +1,57 @@
-calcDisplay = document.querySelector(".display span");
-decBtn = document.getElementById("dec");
+const calcStack = [];
+const calcDisplay = document.querySelector(".display span");
+const histDisplay = document.getElementById("hist");
+const decBtn = document.getElementById("dec");
+let oper;
+
+const sum = (a,b) => a + b;
+const diff = (a,b) => a - b;
+const prod = (a,b) => a * b;
+const quot = (a,b) => b == 0 ? calcDisplay.textContent = "nope" : a / b;
+
+const calc = () => {
+    calcStack.push(calcDisplay.textContent);
+    if (calcStack.length > 1) calcDisplay.textContent = oper(+calcStack[calcStack.length - 2], +calcStack[calcStack.length - 1]);
+};
 
 function getInput(input) {
     switch (input) {
         case "<":
+            calcStack.length = 0;
             calcDisplay.textContent = "0";
+            histDisplay.textContent = "";
             break;
         case "+":
-            calcDisplay.textContent = "not implementeded";
+            if (histDisplay.textContent) calc();
+            oper = sum;
+            calcStack.push(calcDisplay.textContent);
+            calcDisplay.textContent = "";
+            histDisplay.textContent = `${calcStack[calcStack.length - 1]} + `;
             break;
         case "-":
-            calcDisplay.textContent = "not implementeded";
+            if (histDisplay.textContent) calc();
+            oper = diff;
+            calcStack.push(calcDisplay.textContent);
+            calcDisplay.textContent = "";
+            histDisplay.textContent = `${calcStack[calcStack.length - 1]} - `;
             break;
         case "*":
-            calcDisplay.textContent = "not implementeded";
+            if (histDisplay.textContent) calc();
+            oper = prod;
+            calcStack.push(calcDisplay.textContent);
+            calcDisplay.textContent = "";
+            histDisplay.textContent = `${calcStack[calcStack.length - 1]} * `;
             break;
         case "/":
-            calcDisplay.textContent = "not implementeded";
+            if (histDisplay.textContent) calc();
+            oper = quot;
+            calcStack.push(calcDisplay.textContent);
+            calcDisplay.textContent = "";
+            histDisplay.textContent = `${calcStack[calcStack.length - 1]} / `;
             break;
         case "=":
-            calcDisplay.textContent = "not implementeded";
+            calc();
+            histDisplay.textContent = "";
             break;
         case ".":
             calcDisplay.textContent.includes(".")
