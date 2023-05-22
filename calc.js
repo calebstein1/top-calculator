2a5ba643131c0d8d92c33fd26d6f26568635fb35
Caleb Stein <caleb@calebstein.net>
2023-05-22 14:20:42 -0700

    streamlined input switch into a single function

--- calc.js
+++ calc.js
@@ -1,8 +1,8 @@
 calcDisplay = document.querySelector(".display span");
+decBtn = document.getElementById("dec");
 
-document.querySelectorAll("button").forEach(button => button.addEventListener("click", () => {
-    if (calcDisplay.textContent === "0") calcDisplay.textContent = "";
-    switch (button.textContent) {
+function getInput(input) {
+    switch (input) {
         case "<":
             calcDisplay.textContent = "0";
             break;
@@ -12,7 +12,7 @@ document.querySelectorAll("button").forEach(button => button.addEventListener("c
         case "-":
             calcDisplay.textContent = "not implementeded";
             break;
-        case "x":
+        case "*":
             calcDisplay.textContent = "not implementeded";
             break;
         case "/":
@@ -21,7 +21,25 @@ document.querySelectorAll("button").forEach(button => button.addEventListener("c
         case "=":
             calcDisplay.textContent = "not implementeded";
             break;
+        case ".":
+            calcDisplay.textContent.includes(".")
+                ? null
+                : calcDisplay.textContent += ".";
+            break;
         default:
-            calcDisplay.textContent += button.textContent;
+            if (calcDisplay.textContent === "0") calcDisplay.textContent = "";
+            if (Number(input) || input == "0") calcDisplay.textContent += input;
     }
+    calcDisplay.textContent.includes(".")
+        ? decBtn.setAttribute("disabled", "disabled")
+        : decBtn.removeAttribute("disabled");
+
+}
+
+window.addEventListener("keypress", (event) => {
+    getInput(event.key);
+});
+
+document.querySelectorAll("button").forEach(button => button.addEventListener("click", () => {
+    getInput(button.textContent);
 }));
